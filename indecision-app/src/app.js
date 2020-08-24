3731ac1400d551200808303432e3ac8e8d22d38a
AlisonGraham75 <alison.graham10@gmail.com>
2020-08-24 12:37:00 -0600

    Realtime databinding with ReactDom

--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -19,28 +19,40 @@ const template = (
 </div>
 );
 
-//New JSX expression
-const user = {
-    name: 'Andrew',
-    age: 26,
-    location: 'New York'
+let count = 0;
+const addOne =  () => {
+    count++;
+    renderCounterApp();
 };
-
-function getLocation(location) {
-    if(location) {
-        return <p>Location: {location}</p>;
-    }
+const minusOne = () => {
+    count--;
+    renderCounterApp();
 }
 
-//Rendering dynamic content using ternary operators and logical AND operator
-const templateTwo = (
-    <div>
-        <h1>{user.name ? user.name : 'Anonymous'}</h1>
-        {(user.age && user.age >= 18) && <p>Age: {user.age}</p>} 
-        {getLocation(user.location)}
-    </div>
-);
+const reset = () => {
+    count = 0;  
+    renderCounterApp();
+}
+
+
 
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+
+
+const renderCounterApp = () => {
+
+    const templateTwo = (
+        <div>
+            <h1>Count: {count}</h1>
+            <button onClick={addOne}>+1</button>
+            <button onClick={minusOne}>-1</button>
+            <button onClick={reset}>Reset</button>
+        </div> 
+    );
+
+    ReactDOM.render(templateTwo, appRoot);
+
+};
+
+renderCounterApp();
