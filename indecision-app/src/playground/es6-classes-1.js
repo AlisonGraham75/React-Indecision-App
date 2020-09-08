2b7f402604f587b5006f4f3bb1d3edcd203317fe
AlisonGraham75 <alison.graham10@gmail.com>
2020-09-08 15:34:56 -0600

    added subclasses

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -14,8 +14,46 @@ class Person {
     }
 }
 
-const me = new Person('Andrew Mead', 27);
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
 
-const other = new Person();
-console.log(other);
+    //override the parent behaviour
+    getDescription() {
+        let description = super.getDescription();
+        if(this.hasMajor()) {
+            description += ` Their major is ${this.major}.`;
+        }
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = supper.getGreeting();
+        if (this.hasHomeLocation()) {
+            greeting += ` I'm visiting from ${homeLocation}`;
+        }
+        return greeting;
+    }
+}
+
+const me = new Student('Andrew Mead', 27, 'Computer Science');
+console.log(me.hasMajor());
+
+const other = new Student();
+console.log(other.hasMajor());
